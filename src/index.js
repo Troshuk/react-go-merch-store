17a16ca78c68c98eff2cef3c16ce2948c82bc31a
Denys Troshuk <Troshuk@gmail.com>
2024-01-15 13:33:48 -0400

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./components/App";
+import { App } from "components/App";
+
 import "modern-normalize";
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter basename="/react-go-merch-store">
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
